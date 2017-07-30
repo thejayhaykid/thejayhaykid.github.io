005a04a3303b5fceffb5244600410e9b49e6e899
Jake Hayes <jayhay28324@gmail.com>
2017-07-30 15:34:04 -0400

    Added more of Passer Rating Calculator

---
+++ js/PRC.js
@@ -0,0 +1,34 @@
+var comp = 0;
+var att = 0;
+var yar = 0;
+var tds = 0;
+var ints = 0;
+
+function fillVars(){
+  console.log("entered fillVars");
+  comp = $("#CompID").val();
+  att = $("#AttID").val();
+  yar = $("#YarID").val();
+  tds = $("#TDID").val();
+  ints = $("#IntID").val();
+}
+
+function calculate() {
+    console.log("entered calculate");
+    fillVars();
+    console.log(comp);
+    var ncaa = 0, nfl = 0;
+    ncaa = ((8.4 * yar) + (330 * tds) + (100 * comp) - (200 * ints)) / (att);
+    var a, b, c, d;
+    a = ((comp / att) - (1 / 3)) * 5;
+    b = ((yar / att) - 3) * (1 / 4);
+    c = (tds / att) * 20;
+    d = 2.375 - ((ints / att) * 25);
+    nfl = ((a + b + c + d) / 6) * 100;
+    var ncaa1 = ncaa.toString();
+    var nfl1 = nfl.toString();
+    document.getElementById("NCAA").innerHTML = 'NCAA: ' + ncaa1;
+    document.getElementById("NFL").innerHTML = 'NFL:  ' + nfl1;
+}
+
+$("#myBtn").click(calculate());
