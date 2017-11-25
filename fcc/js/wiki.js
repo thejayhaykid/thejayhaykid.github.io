5f5c2b88038a85220870ef4ef0f015b42e0d49cb
Jake <JayHay28324@gmail.com>
2017-11-24 20:37:41 -0500

    Trying unsuccessfully to get this wikipedia search page to work

--- fcc/js/wiki.js
+++ fcc/js/wiki.js
@@ -1,13 +1,12 @@
 // Setup global variables
-var searchURL = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=";
-var ronURL = "http://ron-swanson-quotes.herokuapp.com/v2/quotes";
+var searchURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&list=search&srsearch=";
 
-function search() {
-    console.log("searching...")
+function search(event) {
+    console.log("searching...");
     var form = document.forms["search-form"];
     var searchbar = form["q"];
     var query = searchbar.value;
-    searchURL += query + "&format=json";
+    searchURL += query;
     var xhr = new XMLHttpRequest();
     xhr.open('GET', searchURL, true);
     xhr.onload = function() {
@@ -18,7 +17,8 @@ function search() {
         }
     };
     xhr.send();
-    console.log("Sent");
+    console.log("Sent " + JSON.stringify(xhr));
+    event.preventDefault();
 }
 
 function setResults(results) {
