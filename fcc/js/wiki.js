3fee966d057712895b8a9955d9b85aa2297261a0
Jake <JayHay28324@gmail.com>
2017-12-02 00:40:58 -0500

    Somewhat working version of FCC Wikipedia page

--- fcc/js/wiki.js
+++ fcc/js/wiki.js
@@ -1,8 +1,9 @@
 // Setup global variables
 var searchURL = "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&list=search&srsearch=";
+var artBaseURL = "https://en.wikipedia.org/?curid=";
 
 function search(event) {
-    console.log("searching...");
+    //console.log("searching...");
     var form = document.forms["search-form"];
     var searchbar = form["q"];
     var query = searchbar.value;
@@ -17,10 +18,43 @@ function search(event) {
         }
     };
     xhr.send();
-    console.log("Sent " + JSON.stringify(xhr));
-    event.preventDefault();
+    //console.log("Sent " + JSON.stringify(xhr));
+    return false;
 }
 
 function setResults(results) {
-  console.log(results);
+  //console.log(results.query.search);
+  var result_arr = results.query.search;
+  var resultDiv = document.getElementById("results");
+
+  for(var i = 0; i < result_arr.length; i++) {
+    var title = result_arr[i].title;
+    var desc = result_arr[i].snippet;
+    console.log("title: " + title);
+    console.log("desc: " + desc);
+    console.log("URL: " + artBaseURL + result_arr[i].pageid);
+
+    var titleNode = document.createTextNode(title);
+    var titleH3 = document.createElement("h3");
+    var descNode = document.createTextNode(desc);
+    var descP = document.createElement("p");
+    var rowDiv = document.createElement("div");
+    var colDiv = document.createElement("div");
+    var href = document.createElement("a");
+
+    rowDiv.classList.add("row");
+    colDiv.classList.add("col-xs-12");
+    colDiv.classList.add("search_result");
+    href.href = artBaseURL + result_arr[i].pageid;
+    href.target = "_blank";
+
+    titleH3.appendChild(titleNode);
+    descP.appendChild(descNode);
+
+    colDiv.appendChild(titleH3);
+    colDiv.appendChild(descP);
+    href.appendChild(colDiv);
+    rowDiv.appendChild(href);
+    resultDiv.appendChild(rowDiv);
+  }
 }
